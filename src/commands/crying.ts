a72c2f29d86661b185f44a224e5206345b08545a
Ghost_bat_101 <77197454+GhostBat101@users.noreply.github.com>
2021-10-09 21:00:17 +0600

    Added crying reaction

---
+++ src/commands/crying.ts
@@ -0,0 +1,32 @@
+// ====================
+// SECTION | IMPORTS
+// ====================
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { CommandInteraction, GuildMember, MessageEmbed } from 'discord.js';
+import { crying } from '@assets/json/reaction-gifs.json';
+import _ from 'lodash';
+// ====================!SECTION
+
+// ====================
+// SECTION | COMMAND
+// ====================
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('crying')
+    .setDescription('Crying :)'),
+  async execute(interaction: CommandInteraction) {
+    await interaction.reply({
+      embeds: [
+        new MessageEmbed({
+          title: `${
+            (interaction.member! as GuildMember).displayName
+          } is Crying`,
+          image: {
+            url: _.sample(crying),
+          },
+        }),
+      ],
+    });
+  },
+};
+// ====================!SECTION
