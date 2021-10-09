82a7b0a70a5228d1d5c1cac4083866c46aaaf382
Ghost_bat_101 <77197454+GhostBat101@users.noreply.github.com>
2021-10-09 21:01:23 +0600

    Added smirk reaction

---
+++ src/commands/smirk.ts
@@ -0,0 +1,32 @@
+// ====================
+// SECTION | IMPORTS
+// ====================
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { CommandInteraction, GuildMember, MessageEmbed } from 'discord.js';
+import { smirk } from '@assets/json/reaction-gifs.json';
+import _ from 'lodash';
+// ====================!SECTION
+
+// ====================
+// SECTION | COMMAND
+// ====================
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('smirk')
+    .setDescription('Smirk :)'),
+  async execute(interaction: CommandInteraction) {
+    await interaction.reply({
+      embeds: [
+        new MessageEmbed({
+          title: `${
+            (interaction.member! as GuildMember).displayName
+          } is Smirking`,
+          image: {
+            url: _.sample(smirk),
+          },
+        }),
+      ],
+    });
+  },
+};
+// ====================!SECTION
