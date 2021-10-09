ce856f6177da80e8cc081913392c02bb2e95b567
Ghost_bat_101 <77197454+GhostBat101@users.noreply.github.com>
2021-10-09 20:51:08 +0600

    Added scream reaction

---
+++ src/commands/scream.ts
@@ -0,0 +1,32 @@
+// ====================
+// SECTION | IMPORTS
+// ====================
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { CommandInteraction, GuildMember, MessageEmbed } from 'discord.js';
+import { scream } from '@assets/json/reaction-gifs.json';
+import _ from 'lodash';
+// ====================!SECTION
+
+// ====================
+// SECTION | COMMAND
+// ====================
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('scream')
+    .setDescription('Scream :)'),
+  async execute(interaction: CommandInteraction) {
+    await interaction.reply({
+      embeds: [
+        new MessageEmbed({
+          title: `${
+            (interaction.member! as GuildMember).displayName
+          } is Screaming`,
+          image: {
+            url: _.sample(scream),
+          },
+        }),
+      ],
+    });
+  },
+};
+// ====================!SECTION
