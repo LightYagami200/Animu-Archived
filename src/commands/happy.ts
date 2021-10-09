dd53b81700188b08a08ace3e4c2ab04efa0fd706
Ghost_bat_101 <77197454+GhostBat101@users.noreply.github.com>
2021-10-09 20:49:37 +0600

    Added happy reaction

---
+++ src/commands/happy.ts
@@ -0,0 +1,32 @@
+// ====================
+// SECTION | IMPORTS
+// ====================
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { CommandInteraction, GuildMember, MessageEmbed } from 'discord.js';
+import { happy } from '@assets/json/reaction-gifs.json';
+import _ from 'lodash';
+// ====================!SECTION
+
+// ====================
+// SECTION | COMMAND
+// ====================
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('happy')
+    .setDescription('Happy :)'),
+  async execute(interaction: CommandInteraction) {
+    await interaction.reply({
+      embeds: [
+        new MessageEmbed({
+          title: `${
+            (interaction.member! as GuildMember).displayName
+          } is Happy`,
+          image: {
+            url: _.sample(happy),
+          },
+        }),
+      ],
+    });
+  },
+};
+// ====================!SECTION
