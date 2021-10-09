b08951281b12f02653871061dc96afd8bcffdcd0
Ghost_bat_101 <77197454+GhostBat101@users.noreply.github.com>
2021-10-09 21:05:42 +0600

    Added idea reaction

---
+++ src/commands/idea.ts
@@ -0,0 +1,32 @@
+// ====================
+// SECTION | IMPORTS
+// ====================
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { CommandInteraction, GuildMember, MessageEmbed } from 'discord.js';
+import { idea } from '@assets/json/reaction-gifs.json';
+import _ from 'lodash';
+// ====================!SECTION
+
+// ====================
+// SECTION | COMMAND
+// ====================
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('idea')
+    .setDescription('Idea :)'),
+  async execute(interaction: CommandInteraction) {
+    await interaction.reply({
+      embeds: [
+        new MessageEmbed({
+          title: `${
+            (interaction.member! as GuildMember).displayName
+          } got an idea`,
+          image: {
+            url: _.sample(idea),
+          },
+        }),
+      ],
+    });
+  },
+};
+// ====================!SECTION
