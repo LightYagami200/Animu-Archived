b22d53e143c4de5b76697d5157b5ae13618d7a27
Ghost_bat_101 <77197454+GhostBat101@users.noreply.github.com>
2021-10-09 21:03:02 +0600

    Added evil smile reaction

---
+++ src/commands/evil.ts
@@ -0,0 +1,32 @@
+// ====================
+// SECTION | IMPORTS
+// ====================
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { CommandInteraction, GuildMember, MessageEmbed } from 'discord.js';
+import { evil } from '@assets/json/reaction-gifs.json';
+import _ from 'lodash';
+// ====================!SECTION
+
+// ====================
+// SECTION | COMMAND
+// ====================
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('evil')
+    .setDescription('Evil :)'),
+  async execute(interaction: CommandInteraction) {
+    await interaction.reply({
+      embeds: [
+        new MessageEmbed({
+          title: `${
+            (interaction.member! as GuildMember).displayName
+          } is giving an evil smile`,
+          image: {
+            url: _.sample(evil),
+          },
+        }),
+      ],
+    });
+  },
+};
+// ====================!SECTION
