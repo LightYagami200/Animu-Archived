dfaf2903204ff4baaf6b37f61c30f6b18035d214
Ghost_bat_101 <77197454+GhostBat101@users.noreply.github.com>
2021-10-09 20:52:32 +0600

    Added afraid reaction

---
+++ src/commands/afraid.ts
@@ -0,0 +1,32 @@
+// ====================
+// SECTION | IMPORTS
+// ====================
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { CommandInteraction, GuildMember, MessageEmbed } from 'discord.js';
+import { afraid } from '@assets/json/reaction-gifs.json';
+import _ from 'lodash';
+// ====================!SECTION
+
+// ====================
+// SECTION | COMMAND
+// ====================
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('afraid')
+    .setDescription('Afraid :)'),
+  async execute(interaction: CommandInteraction) {
+    await interaction.reply({
+      embeds: [
+        new MessageEmbed({
+          title: `${
+            (interaction.member! as GuildMember).displayName
+          } is scared`,
+          image: {
+            url: _.sample(afraid),
+          },
+        }),
+      ],
+    });
+  },
+};
+// ====================!SECTION
