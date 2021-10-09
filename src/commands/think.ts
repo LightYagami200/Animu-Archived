d6eda6a83d88375ee6626c38d49caf57754e163a
Ghost_bat_101 <77197454+GhostBat101@users.noreply.github.com>
2021-10-09 20:59:01 +0600

    Added think reaction

---
+++ src/commands/think.ts
@@ -0,0 +1,32 @@
+// ====================
+// SECTION | IMPORTS
+// ====================
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { CommandInteraction, GuildMember, MessageEmbed } from 'discord.js';
+import { think } from '@assets/json/reaction-gifs.json';
+import _ from 'lodash';
+// ====================!SECTION
+
+// ====================
+// SECTION | COMMAND
+// ====================
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('think')
+    .setDescription('Think :)'),
+  async execute(interaction: CommandInteraction) {
+    await interaction.reply({
+      embeds: [
+        new MessageEmbed({
+          title: `${
+            (interaction.member! as GuildMember).displayName
+          } is thinking`,
+          image: {
+            url: _.sample(think),
+          },
+        }),
+      ],
+    });
+  },
+};
+// ====================!SECTION
