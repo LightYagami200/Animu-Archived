cb4b5a4583608569a9976576cebb351a8070acf7
Ghost_bat_101 <77197454+GhostBat101@users.noreply.github.com>
2021-10-11 00:28:52 +0600

    cuddle command added

---
+++ src/commands/cuddle.ts
@@ -0,0 +1,83 @@
+// ====================
+// SECTION | IMPORTS
+// ====================
+import { SlashCommandBuilder } from '@discordjs/builders';
+import {
+  ButtonInteraction,
+  CommandInteraction,
+  GuildMember,
+  MessageEmbed,
+} from 'discord.js';
+import { cuddle } from '@assets/json/action-gifs.json';
+import _ from 'lodash';
+import confirm from '@utils/confirm';
+// ====================!SECTION
+
+// ====================
+// SECTION | COMMAND
+// ====================
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('cuddle')
+    .setDescription('Cuddle someone')
+    .addUserOption((option) =>
+      option
+        .setName('user')
+        .setDescription('User to cuddle')
+        .setRequired(true),
+    ),
+  async execute(interaction: CommandInteraction) {
+    if (
+      (interaction.options.getMember('user') as GuildMember).id ===
+      (interaction.member as GuildMember).id
+    )
+      return interaction.reply({
+        content: "You can't cuddle yourself",
+        ephemeral: true,
+      });
+
+    await interaction.deferReply();
+
+    try {
+      const ir = await confirm(
+        interaction,
+        `${
+          interaction.options.getMember('user') as GuildMember
+        }, Would you like to allow ${
+          (interaction.member as GuildMember).displayName
+        } to cuddle you?`,
+        (interaction.options.getMember('user') as GuildMember).id,
+        60 * 1000,
+      );
+
+      await ir.update({
+        content: '',
+        embeds: [
+          new MessageEmbed({
+            title: `${
+              (interaction.member as GuildMember).displayName
+            } cuddled ${
+              (interaction.options.getMember('user') as GuildMember)
+                .displayName
+            }`,
+            image: {
+              url: _.sample(cuddle),
+            },
+          }),
+        ],
+        components: [],
+      });
+    } catch (e) {
+      await (e as ButtonInteraction).update({
+        content: `Oof! ${
+          (interaction.options.getMember('user') as GuildMember)
+            .displayName
+        } denied cuddle by ${
+          (interaction.member as GuildMember).displayName
+        }`,
+        components: [],
+      });
+    }
+  },
+};
+// ====================!SECTION
