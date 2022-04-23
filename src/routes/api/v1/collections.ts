792582875b2e100a6f3b2418b6f4a89f7e18cd99
Light Yagami <its_lightyagami@icloud.com>
2022-04-23 17:21:28 +0500

    Minor bug fixes

--- src/routes/api/v1/collections.ts
+++ src/routes/api/v1/collections.ts
@@ -16,196 +16,194 @@ const collections = Router();
 // ===========================
 // SECTION | MAIN
 // ===========================
-export default () => {
-  // -> Get All Collections with pagination
-  collections.get(
-    '/',
-    [query('page').isNumeric(), query('limit').isNumeric()],
-    async (req: Request, res: Response) => {
-      const errors = validationResult(req);
+// -> Get All Collections with pagination
+collections.get(
+  '/',
+  [query('page').isNumeric(), query('limit').isNumeric()],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
 
-      if (!errors.isEmpty())
-        return res.status(400).json({ errors: errors.array() });
+    if (!errors.isEmpty())
+      return res.status(400).json({ errors: errors.array() });
 
-      // -> Get query params
-      const { page, limit } = req.query;
+    // -> Get query params
+    const { page, limit } = req.query;
 
-      // -> Apply constraints
-      const pageQ = parseInt(page as string);
-      const limitQ = Math.min(parseInt(limit as string), 20);
-      const offset = (pageQ - 1) * limitQ;
+    // -> Apply constraints
+    const pageQ = parseInt(page as string);
+    const limitQ = Math.min(parseInt(limit as string), 20);
+    const offset = (pageQ - 1) * limitQ;
 
-      const collections = await NFTCollectionModel.find(
-        {
-          status: 'published',
-        },
-        {},
-        {
-          sort: {
-            createdAt: -1,
-          },
-          skip: offset,
-          limit: limitQ,
-        },
-      );
-
-      res.json(collections);
-    },
-  );
-
-  // -> Create a new collection
-  collections.post(
-    '/',
-    validateUser,
-    [
-      body('name').isString().notEmpty(),
-      body('description').isString().notEmpty(),
-    ],
-    async (req: Request, res: Response) => {
-      const errors = validationResult(req);
-
-      if (!errors.isEmpty())
-        return res.status(400).json({ errors: errors.array() });
-
-      // -> Get body properties
-      const { name, description } = req.body;
-
-      // -> Create new collection
-      const collection = await NFTCollectionModel.createNFTCollection(
-        req.user.discord.id,
-        name,
-        description,
-      );
-
-      res.json(collection);
-    },
-  );
-
-  // -> Update a collection
-  collections.put(
-    '/:id',
-    validateUser,
-    [param('id').isMongoId()],
-    async (req: Request, res: Response) => {
-      const errors = validationResult(req);
-
-      if (!errors.isEmpty())
-        return res.status(400).json({ errors: errors.array() });
-
-      // -> Get body properties
-      const { name, description, socials, tags } = req.body;
-
-      // -> Find collection
-      const collection = await NFTCollectionModel.findOne({
-        _id: req.params.id,
-        owner: req.user.discord.id,
-      });
-
-      // -> Check if collection exists
-      if (!collection)
-        return res.status(404).json({
-          errors: [
-            {
-              msg: 'Collection not found',
-            },
-          ],
-        });
-
-      // -> Update collection
-      await collection.updateNFTCollection({
-        name,
-        description,
-        socials,
-        tags,
-      });
-
-      res.json(collection);
-    },
-  );
-
-  // -> Get Collection by ID
-  collections.get(
-    '/:id',
-    [param('id').isNumeric().not().isEmpty()],
-    async (req: Request, res: Response) => {
-      const errors = validationResult(req);
-
-      if (!errors.isEmpty())
-        return res.status(400).json({ errors: errors.array() });
-
-      // -> Get params
-      const { id } = req.params;
-
-      // -> Get collection
-      const collection = await NFTCollectionModel.findOne({
-        _id: id,
+    const collections = await NFTCollectionModel.find(
+      {
         status: 'published',
+      },
+      {},
+      {
+        sort: {
+          createdAt: -1,
+        },
+        skip: offset,
+        limit: limitQ,
+      },
+    );
+
+    res.json(collections);
+  },
+);
+
+// -> Create a new collection
+collections.post(
+  '/',
+  validateUser,
+  [
+    body('name').isString().notEmpty(),
+    body('description').isString().notEmpty(),
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty())
+      return res.status(400).json({ errors: errors.array() });
+
+    // -> Get body properties
+    const { name, description } = req.body;
+
+    // -> Create new collection
+    const collection = await NFTCollectionModel.createNFTCollection(
+      req.user.discord.id,
+      name,
+      description,
+    );
+
+    res.json(collection);
+  },
+);
+
+// -> Get collections of logged in user
+collections.get(
+  '/me',
+  [validateUser],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty())
+      return res.status(400).json({ errors: errors.array() });
+
+    // -> Get collections
+    const collections = await NFTCollectionModel.find({
+      owner: req.user.discord.id,
+    });
+
+    res.json(collections);
+  },
+);
+
+// -> Update a collection
+collections.put(
+  '/:id',
+  validateUser,
+  [param('id').isMongoId()],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty())
+      return res.status(400).json({ errors: errors.array() });
+
+    // -> Get body properties
+    const { name, description, socials, tags } = req.body;
+
+    // -> Find collection
+    const collection = await NFTCollectionModel.findOne({
+      _id: req.params.id,
+      owner: req.user.discord.id,
+    });
+
+    // -> Check if collection exists
+    if (!collection)
+      return res.status(404).json({
+        errors: [
+          {
+            msg: 'Collection not found',
+          },
+        ],
       });
 
-      if (!collection)
-        return res.status(404).json({
-          errors: [
-            {
-              msg: 'Collection not found',
-            },
-          ],
-        });
+    // -> Update collection
+    await collection.updateNFTCollection({
+      name,
+      description,
+      socials,
+      tags,
+    });
 
-      res.json(collection);
-    },
-  );
+    res.json(collection);
+  },
+);
 
-  // -> Get collections of logged in user
-  collections.get(
-    '/me',
-    [validateUser],
-    async (req: Request, res: Response) => {
-      const errors = validationResult(req);
+// -> Get Collection by ID
+collections.get(
+  '/:id',
+  [param('id').isNumeric().not().isEmpty()],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
 
-      if (!errors.isEmpty())
-        return res.status(400).json({ errors: errors.array() });
+    if (!errors.isEmpty())
+      return res.status(400).json({ errors: errors.array() });
 
-      // -> Get collections
-      const collections = await NFTCollectionModel.find({
-        owner: req.user.discord.id,
+    // -> Get params
+    const { id } = req.params;
+
+    // -> Get collection
+    const collection = await NFTCollectionModel.findOne({
+      _id: id,
+      status: 'published',
+    });
+
+    if (!collection)
+      return res.status(404).json({
+        errors: [
+          {
+            msg: 'Collection not found',
+          },
+        ],
       });
 
-      res.json(collections);
-    },
-  );
+    res.json(collection);
+  },
+);
 
-  // -> Get a collection of logged in user by ID
-  collections.get(
-    '/me/:id',
-    [validateUser, param('id').isNumeric().not().isEmpty()],
-    async (req: Request, res: Response) => {
-      const errors = validationResult(req);
+// -> Get a collection of logged in user by ID
+collections.get(
+  '/me/:id',
+  [validateUser, param('id').isNumeric().not().isEmpty()],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
 
-      if (!errors.isEmpty())
-        return res.status(400).json({ errors: errors.array() });
+    if (!errors.isEmpty())
+      return res.status(400).json({ errors: errors.array() });
 
-      // -> Get params
-      const { id } = req.params;
+    // -> Get params
+    const { id } = req.params;
 
-      // -> Get collection
-      const collection = await NFTCollectionModel.findOne({
-        _id: id,
-        owner: req.user.discord.id,
+    // -> Get collection
+    const collection = await NFTCollectionModel.findOne({
+      _id: id,
+      owner: req.user.discord.id,
+    });
+
+    if (!collection)
+      return res.status(404).json({
+        errors: [
+          {
+            msg: 'Collection not found',
+          },
+        ],
       });
 
-      if (!collection)
-        return res.status(404).json({
-          errors: [
-            {
-              msg: 'Collection not found',
-            },
-          ],
-        });
+    res.json(collection);
+  },
+);
 
-      res.json(collection);
-    },
-  );
-
-  return collections;
-};
+export default collections;
 // =========================== !SECTION
--- src/routes/api/v1/users.ts
+++ src/routes/api/v1/users.ts
@@ -27,127 +27,125 @@ const users = Router();
 // ===========================
 // SECTION | MAIN
 // ===========================
-export default () => {
-  // -> Login
-  users.post(
-    '/login',
-    [body('code').not().isEmpty().trim()],
-    async (req: Request, res: Response) => {
-      const errors = validationResult(req);
+// -> Login
+users.post(
+  '/login',
+  [body('code').not().isEmpty().trim()],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
 
-      if (!errors.isEmpty())
-        return res.status(400).json({ errors: errors.array() });
+    if (!errors.isEmpty())
+      return res.status(400).json({ errors: errors.array() });
 
-      try {
-        const params = new URLSearchParams({
-          client_id: discordClientID,
-          client_secret: discordClientSecret,
-          grant_type: 'authorization_code',
-          code: req.body.code.toString(),
-          redirect_uri: discordRedirectURI,
-          scope: 'identify guilds',
-        });
+    try {
+      const params = new URLSearchParams({
+        client_id: discordClientID,
+        client_secret: discordClientSecret,
+        grant_type: 'authorization_code',
+        code: req.body.code.toString(),
+        redirect_uri: discordRedirectURI,
+        scope: 'identify guilds',
+      });
 
-        const resp = await axios.post(
-          'https://discordapp.com/api/oauth2/token',
-          params,
-          {
-            headers: {
-              'Content-Type': 'application/x-www-form-urlencoded',
-            },
+      const resp = await axios.post(
+        'https://discordapp.com/api/oauth2/token',
+        params,
+        {
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
           },
-        );
-
-        const accessToken: string = resp.data.access_token;
-
-        const payload = { accessToken };
-
-        let token = jwt.sign(payload, encryptionSecret, {
-          expiresIn: '7d',
-        });
-
-        return res.status(200).json({ token });
-      } catch (e) {
-        console.log(e);
-        return res.status(401).json();
-      }
-    },
-  );
-
-  // -> Authorize user
-  users.post(
-    '/auth',
-    [validateUser],
-    async (req: Request, res: Response) => {
-      console.log({
-        isInBetaGuild: betaGuilds.some((bG) =>
-          req.user.guilds.find((g) => g.id === bG),
-        ),
-      });
-
-      // -> Is user a beta tester?
-      if (
-        !betaTesters.includes(req.user.discord.id) &&
-        !betaGuilds.some((bG) => req.user.guilds.find((g) => g.id === bG))
-      )
-        return res.status(401).json({
-          error: 'You are not a beta tester.',
-        });
-
-      return res.status(200).json({ user: req.user });
-    },
-  );
-
-  // -> Verify user
-  users.post(
-    '/verify',
-    [
-      validateUser,
-      body('publicKey').isArray().not().isEmpty(),
-      body('signature').isArray().not().isEmpty(),
-    ],
-    async (req: Request, res: Response) => {
-      const errors = validationResult(req);
-
-      if (!errors.isEmpty())
-        return res.status(400).json({ errors: errors.array() });
-
-      console.log({
-        isInBetaGuild: betaGuilds.some((bG) =>
-          req.user.guilds.find((g) => g.id === bG),
-        ),
-      });
-
-      // -> Is user a beta tester?
-      if (
-        !betaTesters.includes(req.user.discord.id) &&
-        !betaGuilds.some((bG) => req.user.guilds.find((g) => g.id === bG))
-      )
-        return res.status(401).json({
-          error: 'You are not a beta tester.',
-        });
-
-      const message = 'Sign below to verify your wallet ≧◡≦';
-      const encodedMessage = new TextEncoder().encode(message);
-      const publicKey = new PublicKey(req.body.publicKey);
-      const signature = new Uint8Array(req.body.signature);
-
-      const valid = sign.detached.verify(
-        encodedMessage,
-        signature,
-        publicKey.toBytes(),
+        },
       );
 
-      if (!valid) return res.status(401).json();
+      const accessToken: string = resp.data.access_token;
 
-      const user = await req.user.user.addPublicKey(publicKey.toString());
+      const payload = { accessToken };
 
-      return res.status(200).json({
-        user,
+      let token = jwt.sign(payload, encryptionSecret, {
+        expiresIn: '7d',
       });
-    },
-  );
 
-  return users;
-};
+      return res.status(200).json({ token });
+    } catch (e) {
+      console.log(e);
+      return res.status(401).json();
+    }
+  },
+);
+
+// -> Authorize user
+users.post(
+  '/auth',
+  [validateUser],
+  async (req: Request, res: Response) => {
+    console.log({
+      isInBetaGuild: betaGuilds.some((bG) =>
+        req.user.guilds.find((g) => g.id === bG),
+      ),
+    });
+
+    // -> Is user a beta tester?
+    if (
+      !betaTesters.includes(req.user.discord.id) &&
+      !betaGuilds.some((bG) => req.user.guilds.find((g) => g.id === bG))
+    )
+      return res.status(401).json({
+        error: 'You are not a beta tester.',
+      });
+
+    return res.status(200).json({ user: req.user });
+  },
+);
+
+// -> Verify user
+users.post(
+  '/verify',
+  [
+    validateUser,
+    body('publicKey').isArray().not().isEmpty(),
+    body('signature').isArray().not().isEmpty(),
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty())
+      return res.status(400).json({ errors: errors.array() });
+
+    console.log({
+      isInBetaGuild: betaGuilds.some((bG) =>
+        req.user.guilds.find((g) => g.id === bG),
+      ),
+    });
+
+    // -> Is user a beta tester?
+    if (
+      !betaTesters.includes(req.user.discord.id) &&
+      !betaGuilds.some((bG) => req.user.guilds.find((g) => g.id === bG))
+    )
+      return res.status(401).json({
+        error: 'You are not a beta tester.',
+      });
+
+    const message = 'Sign below to verify your wallet ≧◡≦';
+    const encodedMessage = new TextEncoder().encode(message);
+    const publicKey = new PublicKey(req.body.publicKey);
+    const signature = new Uint8Array(req.body.signature);
+
+    const valid = sign.detached.verify(
+      encodedMessage,
+      signature,
+      publicKey.toBytes(),
+    );
+
+    if (!valid) return res.status(401).json();
+
+    const user = await req.user.user.addPublicKey(publicKey.toString());
+
+    return res.status(200).json({
+      user,
+    });
+  },
+);
+
+export default users;
 // =========================== !SECTION
